7d8fa8e36d2ec5821d7b48ef2d0c2aa3632b206d
ETiV <yokfran@gmail.com>
2014-08-13 19:25:28 +0100

    Add `process.version` into the simple test project.

--- node-project/app.js
+++ node-project/app.js
@@ -6,7 +6,8 @@ app.get('/', function(req, res){
 });
 
 app.listen(3000, function(){
-  console.log('Server Started ...');
+  console.log('Server Started ... Access through: http://127.0.0.1:3000/');
+  console.log('Node.js Version:', process.version);
   var i = 0;
   setInterval(function(){
     console.log(i++, 'Server is alive ... to test, run `curl http://127.1:3000/`');
